f652638c3165ec6b19a99dd6285aad6ecda9f3d8
Isaac58222 <125743110+Isaac58222@users.noreply.github.com>
2023-05-04 15:50:44 +0530

    Update cpu-health-check.py

--- cpu-health-check.py
+++ cpu-health-check.py
@@ -5,7 +5,8 @@
 #!/bin/bash/env python3
 import psutil
 def check_cpu_usage(percent):
-  usage = psutil.cpu_percent()
+  usage = psutil.cpu_percent(1)
+  print('DEBUG: usage: {}'.format(usage))
   return usage < percent
 if not check_cpu_usage(75):
   print('ERROR! CPU overloaded')
